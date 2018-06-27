36448d1cf1d2023d3568b8877656e0802f2fe2aa
tasneem <you@example.com>
2018-06-27 16:21:31 +0300

    pull dev

--- app/src/main/java/com/noventapp/direct/user/daos/remote/address/AddressRemoteDao.java
+++ app/src/main/java/com/noventapp/direct/user/daos/remote/address/AddressRemoteDao.java
@@ -1,4 +1,52 @@
 package com.noventapp.direct.user.daos.remote.address;
 
-public class AddressRemoteDao  {
+import com.google.android.gms.maps.model.Marker;
+import com.noventapp.direct.user.constants.ApiConstants;
+import com.noventapp.direct.user.data.network.HttpCall;
+import com.noventapp.direct.user.data.network.HttpHelper;
+import com.noventapp.direct.user.model.BaseWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public class AddressRemoteDao implements IAddressRemoteDao {
+
+    private static AddressRemoteDao instance;
+    private AddressClient addressClient;
+
+    public AddressRemoteDao() {
+        addressClient = HttpHelper.getInstance().create(AddressClient.class);
+
+    }
+
+    public static synchronized AddressRemoteDao getInstance() {
+        if (instance == null) {
+            instance = new AddressRemoteDao();
+        }
+        return instance;
+    }
+
+    @Override
+    public HttpCall<BaseWrapper> createAddress(String addressName, Integer buildingNum
+            , Integer floorNum, Integer apartmentNum, Double lat, Double lng, Integer customerId) {
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("addressName",addressName);
+        map.put("buildingNumber",buildingNum);
+        map.put("floorNumber",floorNum);
+        map.put("apartmentNumber",apartmentNum);
+        map.put("latitude",lat);
+        map.put("longtitude",lng);
+        map.put("customerId",customerId);
+        return addressClient.createAddress(map);
+    }
+
+    private interface AddressClient
+    {
+        @POST(ApiConstants.CREATE_ADDRESS)
+        HttpCall<BaseWrapper> createAddress(@Body Map<String,Object> map);
+    }
 }
--- app/src/main/java/com/noventapp/direct/user/daos/remote/address/IAddressRemoteDao.java
+++ app/src/main/java/com/noventapp/direct/user/daos/remote/address/IAddressRemoteDao.java
@@ -5,7 +5,8 @@ import com.noventapp.direct.user.model.BaseWrapper;
 
 public interface IAddressRemoteDao {
 
-    HttpCall<BaseWrapper> createAddress(String addressName);
+    HttpCall<BaseWrapper> createAddress(String addressName, Integer buildingNum,Integer floorNum,
+                                        Integer apartmentNum,Double lat,Double lng ,Integer customerId);
 //    HttpCall<> getAddressList();
 //    HttpCall<> deleteAddress();
 //    HttpCall<> updateAddress();
