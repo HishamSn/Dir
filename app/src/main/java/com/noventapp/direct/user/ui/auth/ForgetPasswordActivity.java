52b20f758923b84f69117c3b2ca3e3bb25effef2
tasneem <you@example.com>
2018-07-01 10:18:53 +0300

    pull dev

--- app/src/main/java/com/noventapp/direct/user/ui/auth/ForgetPasswordActivity.java
+++ app/src/main/java/com/noventapp/direct/user/ui/auth/ForgetPasswordActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
 
 import com.noventapp.direct.user.R;
+import com.noventapp.direct.user.daos.remote.auth.UserRemoteDao;
+import com.noventapp.direct.user.data.network.HttpStatus;
+import com.noventapp.direct.user.utils.DialogUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class ForgetPasswordActivity extends AppCompatActivity {
+public class ForgetPasswordActivity extends AppCompatActivity implements View.OnFocusChangeListener {
 
     @BindView(R.id.et_email)
     AppCompatEditText etEmail;
@@ -32,5 +35,42 @@ public class ForgetPasswordActivity extends AppCompatActivity {
             case R.id.btn_send:
                 break;
         }
+    }
+
+    private void userDaoEmail(String email) {
+        UserRemoteDao.getInstance().checkEmail(email)
+                .enqueue(result -> {
+
+                    switch (result.getStatus()) {
+                        case HttpStatus.SUCCESS:
+                            DialogUtil.errorMessage(this, result.getResult().getMessage());
+                            break;
+
+                        case HttpStatus.BAD_REQUEST:
+                            // DialogUtil.errorMessage(this, result.getError().getMessage());
+                            break;
+
+                        case HttpStatus.SERVER_ERROR:
+                            DialogUtil.errorMessage(this, getString(R.string.server_error));
+                            break;
+
+                        case HttpStatus.NETWORK_ERROR:
+                            DialogUtil.errorMessage(this, getString(R.string.network_error));
+                            break;
+
+                        default:
+                            DialogUtil.errorMessage(this, getString(R.string.unexpected_error));
+                            break;
+                    }
+
+                });
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+            userDaoEmail(etEmail.getText().toString());
+        }
+
     }
 }
